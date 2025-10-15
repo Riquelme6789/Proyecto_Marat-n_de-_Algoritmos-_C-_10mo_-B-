a9d5705692685fffb4c88a61dd2b601bd7488704
Riquelme6789 <jcunalata@alumno.montepiedra.edu.ec>
2025-10-15 18:37:36 -0500

    Add files via upload

---
+++ ejercicio_001.cpp
@@ -0,0 +1,6 @@
+// Archivo generado: ejercicio_001.cpp
+// Ejercicio: Suma de dos números
+#include <iostream>
+using namespace std;
+int main(){
+ double a,b; cout<<"Ingrese dos numeros: "; if(!(cin>>a>>b)) return 0; cout<<"Suma = "<<(a+b)<<"\n"; return 0; }
