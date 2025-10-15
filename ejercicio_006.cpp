ba4a204e53bedb450fd4e1ebf429deba2519ef50
Riquelme6789 <jcunalata@alumno.montepiedra.edu.ec>
2025-10-15 18:45:53 -0500

    Add files via upload

---
+++ ejercicio_006.cpp
@@ -0,0 +1,5 @@
+// Archivo generado: ejercicio_006.cpp
+// Ejercicio: Convertir Celsius a Fahrenheit
+#include <iostream>
+using namespace std;
+int main(){ double c; cout<<"Ingrese grados Celsius: "; if(!(cin>>c)) return 0; double f = c*9.0/5.0 + 32.0; cout<<"Fahrenheit = "<<f<<"\n"; return 0; }
