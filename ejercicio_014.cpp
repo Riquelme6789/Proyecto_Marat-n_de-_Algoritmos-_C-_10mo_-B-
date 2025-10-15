ef0f4d09f4990c428e1774a6336e1da0dce810a5
Riquelme6789 <jcunalata@alumno.montepiedra.edu.ec>
2025-10-15 18:48:58 -0500

    Add files via upload

---
+++ ejercicio_014.cpp
@@ -0,0 +1,5 @@
+// Archivo generado: ejercicio_014.cpp
+// Ejercicio: Tabla de multiplicar
+#include <iostream>
+using namespace std;
+int main(){ int n; if(!(cin>>n)) return 0; for(int i=1;i<=10;i++) cout<<n<<" x "<<i<<" = "<<n*i<<"\n"; return 0; }
